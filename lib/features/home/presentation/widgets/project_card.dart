aadca13cf4020f5054729c18323fadc9b3abf86a
Nicolas Esteban <120282675+Nekolas777@users.noreply.github.com>
2024-09-25 15:29:38 -0500

    feat: home_screen v1 added with reusable widgets

---
+++ lib/features/home/presentation/widgets/project_card.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class ProjectCard extends StatelessWidget {
+  const ProjectCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.grey,
+      child: Stack(
+        children: [
+          Stack(
+            children: [
+              Image.asset(
+                'assets/images/project-example.jpg',
+                fit: BoxFit.cover,
+                width: double.infinity,
+                height: double.infinity,
+              ),
+              Container(
+                width: double.infinity,
+                height: double.infinity,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter,
+                    colors: [
+                      Colors.black.withOpacity(0.70),
+                      Colors.transparent,
+                    ],
+                  ),
+                ),
+              ),
+              const Positioned(
+                bottom: 15,
+                left: 20.0,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Recolecting Plants',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24.0,
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 1.05),
+                    ),
+                    SizedBox(height: 5.0), // Espacio entre los textos
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.person,
+                          color: Colors.white,
+                          size: 24.0,
+                        ),
+                        SizedBox(
+                            width: 8.0), // Espacio entre el ícono y el texto
+                        Text(
+                          '25 members',
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 214, 214, 214),
+                            fontSize: 18.0,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
