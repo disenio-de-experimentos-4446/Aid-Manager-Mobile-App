464ce5b857e8fdbd5e403da26f1419b51be66d02
Nicolas Esteban <120282675+Nekolas777@users.noreply.github.com>
2024-10-22 11:02:43 -0500

    feat(design): auth feature finished with dialogs for errors

--- lib/features/auth/presentation/widgets/login/dialog/login_error_dialog.dart
+++ lib/features/auth/presentation/widgets/login/dialog/login_error_dialog.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class InvalidEmailDialog extends StatelessWidget {
-  const InvalidEmailDialog({super.key});
+class LoginErrorDialog extends StatelessWidget {
+  const LoginErrorDialog({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -10,34 +10,20 @@ class InvalidEmailDialog extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Stack(
-            alignment: Alignment.center,
-            children: [
-              Icon(
-                Icons.mail,
-                color: Color.fromARGB(255, 44, 44, 44),
-                size: 72,
-              ),
-              Positioned(
-                right: 0,
-                bottom: 3,
-                child: Icon(
-                  Icons.cancel,
-                  color: Color.fromARGB(255, 241, 99, 88),
-                  size: 30,
-                ),
-              ),
-            ],
+          Icon(
+            Icons.error_outline,
+            color: Colors.red,
+            size: 72,
           ),
           SizedBox(height: 20),
           Text(
-            'Invalid Email',
+            'Login Failed',
             style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
           ),
         ],
       ),
       content: const Text(
-        'The email you entered is not valid.\nPlease try again.',
+        'The credentials are not valid.\nPlease try again.',
         textAlign: TextAlign.center,
         style: TextStyle(fontSize: 18, height: 1.65),
       ),
