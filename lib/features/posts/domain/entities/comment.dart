fa77d3016fd695d7b6292b66c9ca1710ca76ea09
U202210138 <129425974+U202210138@users.noreply.github.com>
2024-10-24 17:56:22 -0500

    feat: added comment entity

---
+++ lib/features/posts/domain/entities/comment.dart
@@ -0,0 +1,12 @@
+class Comment {
+  final int? id;
+  final String comment;
+  final int? userId;
+  final String? userName;
+  final String? userEmail;
+  final String? userImage;
+  final int postId;
+  final String? commentTime;
+
+
+}
