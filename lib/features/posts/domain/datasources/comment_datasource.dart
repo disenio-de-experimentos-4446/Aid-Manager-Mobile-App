89ec1364a00c4bd4bd847e34fdcb55f8bc32e3af
U202210138 <129425974+U202210138@users.noreply.github.com>
2024-10-24 18:00:12 -0500

    feat: added comment datasource delete

--- lib/features/posts/domain/datasources/comment_datasource.dart
+++ lib/features/posts/domain/datasources/comment_datasource.dart
@@ -3,4 +3,5 @@ import 'package:aidmanager_mobile/features/posts/domain/entities/comment.dart';
 abstract class CommentDatasource {
   Future<List<Comment>> getAllCommentsFromPost(int postId);
   Future<void> createCommentByPostId(int postId, Comment comment);
+  Future<void> deleteCommentByPostIdAndCommentId(int postId, int commentId);
 }
