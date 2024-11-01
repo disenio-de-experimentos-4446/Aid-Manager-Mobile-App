ab3169b9bf26c9dcfe7a0b0876d24e84911ce9ac
Manuel Peña <u202210138@upc.edu.pe>
2024-11-01 16:30:21 -0500

    feat: updated user_repository.dart

--- lib/features/profile/domain/repositories/user_repository.dart
+++ lib/features/profile/domain/repositories/user_repository.dart
@@ -1,10 +1,8 @@
+
 import 'package:aidmanager_mobile/features/profile/domain/entities/user.dart';
 
 abstract class UserRepository {
-  Future<List<User>> getAllUsers();
   Future<User> getUserById(int id);
   Future<List<User>> getAllUsersByCompanyId(int companyId);
-  Future<List<User>> getMembersByCompanyName(String companyName);
-  Future<User> getDirectorByCompanyName(String companyName);
   Future<void> deleteUserById(int userId);
 }
