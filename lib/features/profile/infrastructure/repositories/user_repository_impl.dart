17d562fb5af30c6589ae5fec3c70b5f86a0c5272
Manuel Peña <u202210138@upc.edu.pe>
2024-11-01 16:30:30 -0500

    feat: updated user_repository_impl.dart

--- lib/features/profile/infrastructure/repositories/user_repository_impl.dart
+++ lib/features/profile/infrastructure/repositories/user_repository_impl.dart
@@ -1,33 +1,18 @@
+// lib/features/profile/infrastructure/repositories/user_repository_impl.dart
 import 'package:aidmanager_mobile/features/profile/domain/datasources/user_datasource.dart';
 import 'package:aidmanager_mobile/features/profile/domain/entities/user.dart';
 import 'package:aidmanager_mobile/features/profile/domain/repositories/user_repository.dart';
 
 class UserRepositoryImpl implements UserRepository {
-
   final UserDatasource datasource;
 
   UserRepositoryImpl({required this.datasource});
-  
-  @override
-  Future<List<User>> getAllUsers() {
-    return datasource.getAllUsers();
-  }
-  
+
   @override
   Future<List<User>> getAllUsersByCompanyId(int companyId) {
-    return datasource.getAllUsersByCompanyId(companyId  );
+    return datasource.getAllUsersByCompanyId(companyId);
   }
-  
-  @override
-  Future<User> getDirectorByCompanyName(String companyName) {
-    return datasource.getDirectorByCompanyName(companyName);
-  }
-  
-  @override
-  Future<List<User>> getMembersByCompanyName(String companyName) {
-    return datasource.getMembersByCompanyName(companyName);
-  }
-  
+
   @override
   Future<User> getUserById(int id) {
     return datasource.getUserById(id);
@@ -37,6 +22,4 @@ class UserRepositoryImpl implements UserRepository {
   Future<void> deleteUserById(int userId) {
     return datasource.deleteUserById(userId);
   }
-
-  
 }
