f069e51ed6cc3bc375f5e1f0b4c0ac12fd4fd4d1
Manuel Peña <u202210138@upc.edu.pe>
2024-11-01 16:30:14 -0500

    feat: updated user_datasource_impl.dart

--- lib/features/profile/infrastructure/datasources/user_datasource_impl.dart
+++ lib/features/profile/infrastructure/datasources/user_datasource_impl.dart
@@ -7,22 +7,6 @@ import 'package:aidmanager_mobile/shared/service/http_service.dart';
 class UserDatasourceImpl extends HttpService implements UserDatasource {
 
   @override
-  Future<List<User>> getAllUsers() async {
-    try {
-      final response = await dio.get('/users');
-
-      if (response.statusCode == HttpStatus.ok) {
-        final List<dynamic> usersJson = response.data;
-        return usersJson.map((json) => UserMapper.fromJson(json)).toList();
-      } else {
-        throw Exception('Failed to fetch users: ${response.statusCode}');
-      }
-    } catch (e) {
-      throw Exception('Failed to fetch all users: $e');
-    }
-  }
-
-  @override
   Future<List<User>> getAllUsersByCompanyId(int companyId) async {
     try {
       final response = await dio.get('/users/$companyId');
@@ -39,42 +23,9 @@ class UserDatasourceImpl extends HttpService implements UserDatasource {
   }
 
   @override
-  Future<User> getDirectorByCompanyName(String companyName) async {
-    try {
-      final response = await dio.get('/users/company/$companyName/director');
-
-      if (response.statusCode == HttpStatus.ok) {
-        final dynamic userJson = response.data;
-        return UserMapper.fromJson(userJson);
-      } else {
-        throw Exception('Failed to fetch director: ${response.statusCode}');
-      }
-    } catch (e) {
-      throw Exception('Failed to fetch director by company $companyName: $e');
-    }
-  }
-
-  @override
-  Future<List<User>> getMembersByCompanyName(String companyName) async {
-    try {
-      final response = await dio.get('/users/company/$companyName/members');
-
-      if (response.statusCode == HttpStatus.ok) {
-        final List<dynamic> usersJson = response.data;
-        return usersJson.map((json) => UserMapper.fromJson(json)).toList();
-      } else {
-        throw Exception('Failed to fetch members: ${response.statusCode}');
-      }
-    } catch (e) {
-      throw Exception('Failed to fetch members by $companyName: $e');
-    }
-  }
-
-  @override
   Future<User> getUserById(int id) async {
     try {
       final response = await dio.get('/users/user/$id');
-
 
       if (response.statusCode == HttpStatus.ok) {
         final dynamic userJson = response.data;
@@ -90,7 +41,7 @@ class UserDatasourceImpl extends HttpService implements UserDatasource {
   @override
   Future<void> deleteUserById(int userId) async {
     try {
-      final response = await dio.delete('users/kick-member/$userId');
+      final response = await dio.delete('/users/kick-member/$userId');
       if (response.statusCode != HttpStatus.noContent) {
         throw Exception('Failed to delete user');
       }
