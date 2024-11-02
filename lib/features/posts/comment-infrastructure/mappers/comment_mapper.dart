b32bfa0a5eca41f1b1949a6f5410207f392ea8f1
SEBRH <u202211894@upc.edu.pe>
2024-11-01 21:15:04 -0500

    feat(comment): comment mapper

--- lib/features/posts/comment-infrastructure/mappers/comment_mapper.dart
+++ lib/features/posts/comment-infrastructure/mappers/comment_mapper.dart
@@ -4,26 +4,21 @@ class CommentMapper {
   static Comment fromJson(Map<String, dynamic> json) {
     return Comment(
       id: json['id'],
-      postId: json['postId'], // Ensure this is an int
-      userId: json['userId'],
       comment: json['comment'],
+      userId: json['userId'],
       userName: json['userName'],
       userEmail: json['userEmail'],
       userImage: json['userImage'],
-      commentTime: json['commentTime'],
+      postId: json['postId'],
+      commentTime: DateTime.parse(json['commentTime']),
     );
   }
 
   static Map<String, dynamic> toJson(Comment comment) {
     return {
-      'id': comment.id,
-      'postId': comment.postId, // Ensure this is an int
       'userId': comment.userId,
       'comment': comment.comment,
-      'userName': comment.userName,
-      'userEmail': comment.userEmail,
-      'userImage': comment.userImage,
-      'commentTime': comment.commentTime,
+
     };
   }
 }
