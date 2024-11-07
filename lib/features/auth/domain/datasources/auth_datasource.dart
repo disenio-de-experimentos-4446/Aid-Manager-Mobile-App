31a34202a4d6c3b7776f97d9ee3ac399a6684224
H4RRY73 <103443967+H4RRY73@users.noreply.github.com>
2024-11-07 13:01:58 -0500

    feat(auth): created abstract datasource for handling authentication data requests

--- lib/features/auth/domain/datasources/auth_datasource.dart
+++ lib/features/auth/domain/datasources/auth_datasource.dart
@@ -1,7 +1,6 @@
 import 'package:aidmanager_mobile/features/auth/domain/entities/login_response.dart';
 import 'package:aidmanager_mobile/features/profile/domain/entities/user.dart';
 
-// TODO: "implementar";
 abstract class AuthDatasource {
   Future<LoginResponse> signIn(String email, String password);
 
