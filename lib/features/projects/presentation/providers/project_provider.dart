393e126c87ecaf0fccc2a654513dc53a52106a46
Nicolas Esteban <120282675+Nekolas777@users.noreply.github.com>
2024-11-17 17:18:10 -0500

    feat: methods added for project provider to call methods defined in domain and infra layer

--- lib/features/projects/presentation/providers/project_provider.dart
+++ lib/features/projects/presentation/providers/project_provider.dart
@@ -9,25 +9,42 @@ class ProjectProvider extends ChangeNotifier {
   final ProjectsRepository projectsRepository;
   AuthProvider authProvider;
 
-  ProjectProvider({required this.authProvider, required this.projectsRepository});
+  ProjectProvider({
+    required this.authProvider,
+    required this.projectsRepository,
+  });
 
   bool initialLoading = true;
   bool isLoading = false;
   List<Project> projects = [];
+  List<Project> favProjects = [];
+  List<Project> projectsUser = [];
   Project? projectDetail;
   bool detailLoading = false;
 
+  // la logica para los favorites es bien ratona(basc un load de los proyectos q estan como favoritos
+  // en base a ello se setean en el campo "isFavorite" para cada project)
   Future<void> loadInitialProjects() async {
     initialLoading = true;
+    final userIdLogged = authProvider.user!.id!;
     final companyId = authProvider.user!.companyId!;
 
     try {
       final projectsList = await projectsRepository.getProjectsByCompanyId(companyId);
+      final favProjects =
+          await projectsRepository.getFavoriteProjectsByUser(userIdLogged);
+
+      // cremaos un set de IDs de proyectos favoritos
+      final favProjectIds = favProjects.map((project) => project.id).toSet();
+
+      // actualizamos el campo isFavorite en projectsList
+      for (var project in projectsList) {
+        project.isFavorite = favProjectIds.contains(project.id);
+      }
 
       projects = projectsList;
     } catch (e) {
-      throw ProjectsFetchException(
-          'Failed to fetch projects by company with id: $companyId');
+      throw ProjectsFetchException('Failed to fetch projects by company with id: $companyId');
     } finally {
       initialLoading = false;
       notifyListeners();
@@ -42,7 +59,6 @@ class ProjectProvider extends ChangeNotifier {
     TimeOfDay projectTime,
     String projectLocation,
   ) async {
-
     if (numberImages <= 0 || numberImages > 5) {
       throw InvalidNumberOfImagesException(
           'Number of images must be greater than 0 and less than or equal to 5');
@@ -65,7 +81,6 @@ class ProjectProvider extends ChangeNotifier {
 
     isLoading = true;
     notifyListeners();
-    //print({projectTime});
 
     try {
       await projectsRepository.createProject(newProject);
@@ -73,6 +88,57 @@ class ProjectProvider extends ChangeNotifier {
       throw ProjectCreationException('Failed to create a new project: $e');
     } finally {
       isLoading = false;
+      notifyListeners();
+    }
+  }
+
+  Future<void> updateProject(
+      int projectId,
+      String title,
+      String description,
+      List<String> imageUrl,
+      DateTime projectDate,
+      TimeOfDay projectTime,
+      String projectLocation) async {
+    final loggedCompanyId = authProvider.user!.companyId!;
+
+    if (description.length > 150) {
+      throw InvalidDescriptionLengthException(
+          'The project description must be less than 150 characters');
+    }
+
+    final postToUpdate = Project(
+        name: title,
+        description: description,
+        imageUrl: imageUrl,
+        companyId: loggedCompanyId,
+        projectDate: projectDate,
+        projectTime: projectTime,
+        projectLocation: projectLocation);
+
+    initialLoading = true;
+
+    try {
+      await projectsRepository.updateProjectById(projectId, postToUpdate);
+    } catch (e) {
+      throw ProjectUpdateException(
+          "Error to update current project with id: $projectId, $e");
+    } finally {
+      initialLoading = false;
+      notifyListeners();
+    }
+  }
+
+  Future<void> deleteProjectById(int projectId) async {
+    initialLoading = true;
+
+    try {
+      await projectsRepository.deleteProjectById(projectId);
+      projects.removeWhere((project) => project.id == projectId);
+    } catch (e) {
+      throw Exception('Failed to delete a project with id $projectId: $e');
+    } finally {
+      initialLoading = false;
       notifyListeners();
     }
   }
@@ -89,4 +155,85 @@ class ProjectProvider extends ChangeNotifier {
       notifyListeners();
     }
   }
+
+  Future<void> loadProjectsByUser(int userId) async {
+    initialLoading = true;
+
+    try {
+      final projectsList = await projectsRepository.getProjectsByUser(userId);
+
+      projectsUser = projectsList;
+    } catch (e) {
+      throw ProjectsFetchException(
+          'Failed to fetch projects by user with id: $userId');
+    } finally {
+      initialLoading = false;
+      notifyListeners();
+    }
+  }
+
+  Future<void> saveProjectAsFavorite(int projectId) async {
+    final userIdLogged = authProvider.user!.id!;
+    initialLoading = true;
+
+    try {
+      await projectsRepository.saveProjectAsFavorite(userIdLogged, projectId);
+    } catch (e) {
+      throw ProjectCreationException('Failed to create a new project: $e');
+    } finally {
+      initialLoading = false;
+      notifyListeners();
+    }
+  }
+
+  Future<void> deleteProjectFromFavorites(int projectId) async {
+    final userIdLogged = authProvider.user!.id!;
+    initialLoading = true;
+
+    try {
+      await projectsRepository.deleteProjectFromFavorites(
+          userIdLogged, projectId);
+      // manejamos aqui el array para projects favoritos
+      favProjects.removeWhere((project) => project.id == projectId);
+    } catch (e) {
+      throw ProjectCreationException('Failed to create a new project: $e');
+    } finally {
+      initialLoading = false;
+      notifyListeners();
+    }
+  }
+
+  Future<void> loadFavoritesProjects(int userId) async {
+    initialLoading = true;
+    //final userIdLogged = authProvider.user!.id!;
+    try {
+      final projectsList =
+          await projectsRepository.getFavoriteProjectsByUser(userId);
+
+      favProjects = projectsList;
+    } catch (e) {
+      throw ProjectCreationException('Failed to create a new project: $e');
+    } finally {
+      initialLoading = false;
+      notifyListeners();
+    }
+  }
+
+  Future<void> updateProjectRating(int projectId, double rating) async {
+    detailLoading = true;
+
+    try {
+      await projectsRepository.updateRatingByPost(projectId, rating);
+
+      if (projectDetail != null && projectDetail!.id == projectId) {
+        projectDetail!.rating = rating;
+      }
+    } catch (e) {
+      throw Exception('Failed to update project rating');
+    } finally {
+      detailLoading = false;
+      notifyListeners();
+    }
+  }
+
 }
