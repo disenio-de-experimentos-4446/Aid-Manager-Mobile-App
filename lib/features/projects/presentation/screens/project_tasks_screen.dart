0eb7cd5f43b8d2c0f179c2dd885ce3f0d97540bb
Nicolas Esteban <120282675+Nekolas777@users.noreply.github.com>
2024-11-17 17:28:29 -0500

    feat: new screens for projects created by current user

--- lib/features/projects/presentation/screens/project_tasks_screen.dart
+++ lib/features/projects/presentation/screens/project_tasks_screen.dart
@@ -1,4 +1,5 @@
 import 'package:aidmanager_mobile/config/theme/app_theme.dart';
+import 'package:aidmanager_mobile/features/auth/presentation/providers/auth_provider.dart';
 import 'package:aidmanager_mobile/features/projects/presentation/providers/task_provider.dart';
 import 'package:aidmanager_mobile/features/projects/presentation/widgets/tasks/task_page_view.dart';
 import 'package:flutter/material.dart';
@@ -59,6 +60,7 @@ class _ProjectTasksScreenState extends State<ProjectTasksScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final currentUser = Provider.of<AuthProvider>(context).user;
     final taskProvider = context.watch<TaskProvider>();
 
     return Scaffold(
@@ -93,124 +95,143 @@ class _ProjectTasksScreenState extends State<ProjectTasksScreen> {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              border: Border(
-                bottom: BorderSide(
-                  color: const Color.fromARGB(255, 134, 134, 134),
-                  width: 1.0,
+      body: taskProvider.initialLoading
+          ? Container(
+              color: Colors.black.withOpacity(0.5),
+              child: const Center(
+                child: SizedBox(
+                  width: 80,
+                  height: 80,
+                  child: CircularProgressIndicator(
+                    strokeWidth: 8,
+                    color: CustomColors.darkGreen,
+                  ),
                 ),
               ),
-            ),
-            child: Column(
+            )
+          : Column(
               children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 20, vertical: 25.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(
+                        color: const Color.fromARGB(255, 134, 134, 134),
+                        width: 1.0,
+                      ),
+                    ),
+                  ),
+                  child: Column(
                     children: [
-                      CustomButton(
-                        text: 'To do',
-                        number: taskProvider.tasks
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 20, vertical: 25.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            CustomButton(
+                              text: 'To do',
+                              number: taskProvider.tasks
+                                  .where((task) => task.state == 'ToDo')
+                                  .length,
+                              color: const Color.fromARGB(255, 255, 141, 132),
+                              isSelected: selectedButton == 'To do',
+                              onPressed: () {
+                                handleButtonPress(0, 'To do');
+                              },
+                            ),
+                            SizedBox(width: 20), // Espacio entre botones
+                            CustomButton(
+                              text: 'Progress',
+                              number: taskProvider.tasks
+                                  .where((task) => task.state == 'Progress')
+                                  .length,
+                              color: const Color.fromARGB(255, 229, 255, 0),
+                              isSelected: selectedButton == 'Progress',
+                              onPressed: () {
+                                handleButtonPress(1, 'Progress');
+                              },
+                            ),
+                            SizedBox(width: 20),
+                            CustomButton(
+                              text: 'Complete',
+                              number: taskProvider.tasks
+                                  .where((task) => task.state == 'Done')
+                                  .length,
+                              color: const Color.fromARGB(255, 92, 212, 96),
+                              isSelected: selectedButton == 'Complete',
+                              onPressed: () {
+                                handleButtonPress(2, 'Complete');
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: PageView(
+                    controller: pageController,
+                    onPageChanged: (int page) {
+                      setState(() {
+                        currentPage = page;
+                        switch (page) {
+                          case 0:
+                            selectedButton = 'To do';
+                            break;
+                          case 1:
+                            selectedButton = 'Progress';
+                            break;
+                          case 2:
+                            selectedButton = 'Complete';
+                            break;
+                        }
+                      });
+                    },
+                    children: [
+                      TaskPageView(
+                        stateTitle: "To do",
+                        tasks: taskProvider.tasks
                             .where((task) => task.state == 'ToDo')
-                            .length,
-                        color: const Color.fromARGB(255, 255, 141, 132),
-                        isSelected: selectedButton == 'To do',
-                        onPressed: () {
-                          handleButtonPress(0, 'To do');
-                        },
+                            .toList(),
+                        onUpdateStatus: updateTaskStatus,
+                        onDeleteTask: deleteTaskFromProject,
                       ),
-                      SizedBox(width: 20), // Espacio entre botones
-                      CustomButton(
-                        text: 'Progress',
-                        number: taskProvider.tasks
+                      TaskPageView(
+                        stateTitle: "Progress",
+                        tasks: taskProvider.tasks
                             .where((task) => task.state == 'Progress')
-                            .length,
-                        color: const Color.fromARGB(255, 229, 255, 0),
-                        isSelected: selectedButton == 'Progress',
-                        onPressed: () {
-                          handleButtonPress(1, 'Progress');
-                        },
+                            .toList(),
+                        onUpdateStatus: updateTaskStatus,
+                        onDeleteTask: deleteTaskFromProject,
                       ),
-                      SizedBox(width: 20),
-                      CustomButton(
-                        text: 'Complete',
-                        number: taskProvider.tasks
+                      TaskPageView(
+                        stateTitle: "Complete",
+                        tasks: taskProvider.tasks
                             .where((task) => task.state == 'Done')
-                            .length,
-                        color: const Color.fromARGB(255, 92, 212, 96),
-                        isSelected: selectedButton == 'Complete',
-                        onPressed: () {
-                          handleButtonPress(2, 'Complete');
-                        },
+                            .toList(),
+                        onUpdateStatus: updateTaskStatus,
+                        onDeleteTask: deleteTaskFromProject,
                       ),
                     ],
                   ),
                 ),
               ],
             ),
-          ),
-          Expanded(
-            child: PageView(
-              controller: pageController,
-              onPageChanged: (int page) {
-                setState(() {
-                  currentPage = page;
-                  switch (page) {
-                    case 0:
-                      selectedButton = 'To do';
-                      break;
-                    case 1:
-                      selectedButton = 'Progress';
-                      break;
-                    case 2:
-                      selectedButton = 'Complete';
-                      break;
-                  }
-                });
+      floatingActionButton: (currentUser?.role ?? 'No Role') == 'Manager'
+          ? FloatingActionButton(
+              onPressed: () {
+                context.go(
+                    '/projects/${widget.projectId}/tasks/new?name=${Uri.encodeComponent(widget.projectName)}');
               },
-              children: [
-                TaskPageView(
-                  tasks: taskProvider.tasks
-                      .where((task) => task.state == 'ToDo')
-                      .toList(),
-                  onUpdateStatus: updateTaskStatus,
-                  onDeleteTask: deleteTaskFromProject,
-                ),
-                TaskPageView(
-                  tasks: taskProvider.tasks
-                      .where((task) => task.state == 'Progress')
-                      .toList(),
-                  onUpdateStatus: updateTaskStatus,
-                  onDeleteTask: deleteTaskFromProject,
-                ),
-                TaskPageView(
-                  tasks: taskProvider.tasks
-                      .where((task) => task.state == 'Done')
-                      .toList(),
-                  onUpdateStatus: updateTaskStatus,
-                  onDeleteTask: deleteTaskFromProject,
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          context.go(
-              '/projects/${widget.projectId}/tasks/new?name=${Uri.encodeComponent(widget.projectName)}');
-        },
-        backgroundColor: CustomColors.darkGreen,
-        child: Icon(
-          Icons.add,
-          color: Colors.white,
-          size: 32.0,
-        ),
-      ),
+              backgroundColor: CustomColors.darkGreen,
+              child: Icon(
+                Icons.add,
+                color: Colors.white,
+                size: 32.0,
+              ),
+            )
+          : null,
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
@@ -275,5 +296,3 @@ class CustomButton extends StatelessWidget {
     );
   }
 }
-
-
---
+++ lib/features/projects/presentation/screens/projects_created_by_user_screen.dart
@@ -0,0 +1,208 @@
+import 'package:aidmanager_mobile/config/theme/app_theme.dart';
+import 'package:aidmanager_mobile/features/projects/presentation/providers/project_provider.dart';
+import 'package:aidmanager_mobile/features/projects/presentation/widgets/project/project_user_card.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+
+class ProjectsCreatedByUserScreen extends StatefulWidget {
+  static const String name = "projects_created_by_user_screen";
+  final String userId;
+
+  const ProjectsCreatedByUserScreen({super.key, required this.userId});
+
+  @override
+  State<ProjectsCreatedByUserScreen> createState() =>
+      _ProjectsCreatedByUserScreenState();
+}
+
+class _ProjectsCreatedByUserScreenState
+    extends State<ProjectsCreatedByUserScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _loadProjectsByCurrentUser();
+  }
+
+  Future<void> _loadProjectsByCurrentUser() async {
+    final projectProvider = context.read<ProjectProvider>();
+
+    await projectProvider.loadInitialProjects();
+  }
+
+  Future<void> onDeleteProject(int projectId) async {
+    final projectProvider = context.read<ProjectProvider>();
+
+    await projectProvider.deleteProjectById(projectId);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final projectsProvider =
+        Provider.of<ProjectProvider>(context, listen: true);
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: CustomColors.darkGreen,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+            size: 32.0,
+          ),
+          onPressed: () {
+            context.pop(context);
+          },
+        ),
+        title: Text(
+          'Your Projects',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 22.0,
+          ),
+        ),
+        toolbarHeight: 70.0,
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.filter_list,
+              color: Colors.white,
+              size: 32.0,
+            ),
+            onPressed: () {
+              // Acción al presionar el botón de filtro
+            },
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(22, 72, 255, 21),
+              border: Border(
+                bottom: BorderSide(
+                  color: const Color.fromARGB(255, 172, 169, 169),
+                  width: 1.0,
+                ),
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.only(
+                top: 25.0,
+                left: 20.0,
+                right: 20.0,
+                bottom: 25.0,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: RichText(
+                      text: TextSpan(
+                        text: '',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black87,
+                        ),
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: 'Nicolas',
+                            style: TextStyle(
+                              color: CustomColors.darkGreen,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          TextSpan(
+                            text: ', check your projects created!',
+                            style: TextStyle(
+                              color: Colors.black87,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          projectsProvider.initialLoading
+              ? Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 30.0),
+                  child: Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                )
+              : Expanded(
+                  child: projectsProvider.projectsUser.isEmpty
+                      ? Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.comments_disabled_outlined,
+                                  size: 48, color: Colors.grey),
+                              SizedBox(height: 12),
+                              Text(
+                                'No projects available',
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 18,
+                                ),
+                              ),
+                              SizedBox(height: 20),
+                              ElevatedButton(
+                                onPressed: () {
+                                  context.go('/projects');
+                                },
+                                child: Text(
+                                  'Go to projects',
+                                  style: TextStyle(fontSize: 16.0),
+                                ),
+                              ),
+                            ],
+                          ),
+                        )
+                      : Padding(
+                          padding: const EdgeInsets.only(
+                            left: 20.0,
+                            right: 20.0,
+                            top: 20.0,
+                          ),
+                          child: ListView.builder(
+                            itemCount: projectsProvider.projectsUser.length,
+                            itemBuilder: (context, index) {
+                              final project =
+                                  projectsProvider.projectsUser[index];
+                              return Column(
+                                children: [
+                                  ProjectUserCard(
+                                    projectId: project.id!,
+                                    name: project.name,
+                                    description: project.description,
+                                    imagesUrl: project.imageUrl,
+                                    projectDate: project.projectDate,
+                                    projectTime: project.projectTime,
+                                    projectLocation: project.projectLocation,
+                                    userList: project.userList!,
+                                    onPressedCard: () {
+                                      context.go(
+                                          '/projects/${project.id}?isFavorite=${project.isFavorite}');
+                                    },
+                                    onDeleteFavorite: () =>
+                                        onDeleteProject(project.id!),
+                                  ),
+                                  SizedBox(height: 20.0),
+                                ],
+                              );
+                            },
+                          ),
+                        ),
+                ),
+        ],
+      ),
+    );
+  }
+}
